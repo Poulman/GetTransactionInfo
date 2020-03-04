96fd21c967d82182daed911f875266f5b52b110c
Pavlo <38707599+Poulman@users.noreply.github.com>
2020-03-04 13:59:44 +0200

    Update GetTransactionInfo.sh

--- GetTransactionInfo.sh
+++ GetTransactionInfo.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
 #sudo apt-get install jq
 
-read -p 'Enter Name of Contract: ' contract_name
+read -p 'Enter Name of Contract: ' account
 read -p 'Enter Start DTG  (ISO8601): ' start_dtg
 read -p 'Enter End DTG  (ISO8601):'  end_dtg
 
-#contract_name=ilovekolobok
+#account=ilovekolobok
 #start_dtg=2020-01-28T20:28:56
 #end_dtg=2020-02-01T23:28:56
 
-end="$(date -d "$end_dtg" +"%H:%M:%S %Y-%m-%d")"
-start="$(date -d "$start_dtg" +"%H:%M:%S %Y-%m-%d")"
+end="$(date -d "$end_dtg" "+%s")"
+start="$(date -d "$start_dtg" "+%s")"
 
-limit=10000000000
+steap=86400
+#30 min=1800   1 hour=3800    1 day= 86400
 
-while [ "$start" != "$end" ]; do
-        start1="$(date -d"$start + 30 minutes" +"%H:%M:%S %Y-%m-%d")"
-        a="$(date -d "$start" +"%Y-%m-%dT%H:%M:%S")"
-        b="$(date -d "$start1" +"%Y-%m-%dT%H:%M:%S")"
+limit=100
 
-get_transaction="$(curl -X GET "https://ore.eosusa.news/v2/history/get_actions?limit=$limit&after=$a&before=$b" -H "accept: application/json")"
+while [[ "$start" < "$end" ]]; do
 
-cpu="$(echo $get_transaction | jq '.actions[].cpu_usage_us')"
-        for i in ${cpu[@]}; do
-                let cpu_sum+=$i
-        done
+        start1="$(($start + $steap))"
+        a="$(date -d @"$start" +"%Y-%m-%dT%H:%M:%S")"
+        b="$(date -d @"$start1" +"%Y-%m-%dT%H:%M:%S")"
 
-net="$(echo $get_transaction | jq '.actions[].net_usage_words')"
-        for i in ${net[@]}; do
-                let net_sum+=$i
-        done
-
-len_transaction="$(echo $get_transaction | jq '.actions[].data')"
-        for i in ${len_transaction[@]}; do
-                let transaction_sum+=i
-        done
-
-echo "_______________________________________________"
-start="$(date -d"$start + 30 minutes" +"%H:%M:%S %Y-%m-%d")"
+get_transaction="$(curl -X GET "https://api.eossweden.org/v2/history/get_actions?account=$account&after=$a&before=$b" -H "accept: application/json")"
+start="$(($start + $steap))"
 done
 
-echo "Total CPU: $cpu_sum"
+for row in $(echo "${get_transaction}" | jq -r '.actions[] | @base64'); do
+    _jq() {
+    echo ${row} | base64 --decode | jq -r ${1}
+    }
+    let net+="$(echo $(_jq '.net_usage_words'))"
+    let cpu+="$(echo $(_jq '.cpu_usage_us'))"
+done
 
-echo "Total NET: $net_sum"
+let "net_sum=net_sum*8"
+echo "Total CPU: $cpu"
+echo "Total NET: $net"
 
-echo "Total LEN: $transaction_sum"
